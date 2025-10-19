eea598a35cf4b85ef90d749b20fd1f80c7535f27
MoXcz <130593875+MoXcz@users.noreply.github.com>
2025-10-19 17:14:39 -0600

    feat: include missing email validation endpoint
    Used to validate email on an per-event basis

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
 	mux.HandleFunc("GET /contacts/{contact_id}", getContact)
 	mux.HandleFunc("GET /contacts/{contact_id}/edit", editContactGet)
 	mux.HandleFunc("POST /contacts/{contact_id}/edit", editContactPost)
+	mux.HandleFunc("GET /contacts/{contact_id}/email", getEmailValidation)
 	mux.HandleFunc("DELETE /contacts/{contact_id}", deleteContact)
 
 	srv := http.Server{
